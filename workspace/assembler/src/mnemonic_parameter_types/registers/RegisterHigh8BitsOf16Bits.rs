0f0f71a2d53000c9f835c9d2b4766aa9afc247a8
Raphael Cohn <raphael.cohn@stormmq.com>
2018-09-26 21:30:29 +0100

    Bug fix in high register numbering

--- workspace/assembler/src/mnemonic_parameter_types/registers/RegisterHigh8BitsOf16Bits.rs
+++ workspace/assembler/src/mnemonic_parameter_types/registers/RegisterHigh8BitsOf16Bits.rs
@@ -14,7 +14,7 @@ pub enum RegisterHigh8BitsOf16Bits
 	/// Contains the integer return value from a function call when using the Microsoft x64 Calling Convention.
 	///
 	/// A scratch register (also known as a temporary or (function) caller-saved register) when using the System V Application Binary Interface (ABI) for AMD64.
-	AH = 4,
+	AH = 0,
 	
 	/// Register 1.
 	///
@@ -23,7 +23,7 @@ pub enum RegisterHigh8BitsOf16Bits
 	/// Contains the first integer function argument to a function call when using the Microsoft x64 Calling Convention.
 	///
 	/// A scratch register (also known as a temporary or (function) caller-saved register) when using the System V Application Binary Interface (ABI) for AMD64.
-	CH = 5,
+	CH = 1,
 	
 	/// Register 2.
 	///
@@ -32,13 +32,13 @@ pub enum RegisterHigh8BitsOf16Bits
 	/// Contains the second integer function argument to a function call when using the Microsoft x64 Calling Convention.
 	///
 	/// A scratch register (also known as a temporary or (function) caller-saved register) when using the System V Application Binary Interface (ABI) for AMD64.
-	DH = 6,
+	DH = 2,
 	
 	/// Register 3.
 	///
 	/// A callee-saved register: a called function must preserve the value in this register when using the System V Application Binary Interface (ABI) for AMD64.
 	/// In other words, upon returning, it should make sure the value originally in this register when the call was made is the current value of this register.
-	BH = 7,
+	BH = 3,
 }
 
 impl Default for RegisterHigh8BitsOf16Bits
